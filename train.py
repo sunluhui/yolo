b303260843a6e425af5e4137dcbd670a62fec9a6
slh <2632770017@qq.com>
2025-04-18 17:35:18 +0800

    afpn+asff+head

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # 加载配置
-model = YOLO(model='yolov8n-AFPN4.yaml', task='detect')
+model = YOLO(model='yolov8-simam.yaml', task='detect')
 
 # 训练参数
 model.train(
     data='coco.yaml',  # 数据集配置文件
     epochs=200,
     imgsz=640,
-    batch=12,  # 可设置为更小的，占据更少的空间。
+    batch=16,  # 可设置为更小的，占据更少的空间。
 )
 
