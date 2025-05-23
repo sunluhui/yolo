c0ae21cd5c90719c7d252e770b6e4ccba852a439
slh <2632770017@qq.com>
2025-05-23 13:20:57 +0800

    updatev12

--- convert.py
+++ convert.py
@@ -2,6 +2,6 @@ from ultralytics.data.converter import convert_dota_to_yolo_obb
 
 # 基础转换（自动处理class_mapping）
 convert_dota_to_yolo_obb(
-    dota_path="/home/a10/slh/yolo/datasets/DOTAv1.5/train/labelTxt-v1.5/DOTA-v1.5_train",
-    save_dir="/home/a10/slh/yolo/datasets/DOTAv1.5/train/labelTxt-v1.5/DOTA-v1.5_train_yolo"
+    "/home/a10/slh/yolo/datasets/DOTAv1.5",
+
 )
