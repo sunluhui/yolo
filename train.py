304760439692ae3e48ef7721d4665bd3a6ade371
slh <2632770017@qq.com>
2025-05-24 13:15:06 +0800

    updatev12

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ model = YOLO(model='yolov8.yaml', task='detect')
 # 训练参数
 model.train(
     data='DOTAv1.5.yaml',  # 数据集配置文件
-    epochs=300,
+    epochs=200,
     imgsz=640,
     batch=16,  # 可设置为更小的，占据更少的空间。
 )
