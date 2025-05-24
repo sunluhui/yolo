d52681f982f9b050655569b138affe58421f13e0
slh <2632770017@qq.com>
2025-05-24 10:45:21 +0800

    updatev12

--- a.py
+++ a.py
@@ -1,41 +1,25 @@
 import os
 import shutil
-from tqdm import tqdm  # 可视化进度条
 
+src = '/home/a10/ZH/ultralytics-main/datasets/UAVDT'
+dst = '/home/a10/ZH/ultralytics-main/datasets/UAVDT_cleaned'
 
-def clean_dataset(src_root, dst_root):
-    for split in ['train', 'test']:
-        # 定义路径
-        img_src = os.path.join(src_root, 'images', split)
-        label_src = os.path.join(src_root, 'labels', split)
-        img_dst = os.path.join(dst_root, 'images', split)
-        label_dst = os.path.join(dst_root, 'labels', split)
+for split in ['train', 'test']:  # UAVDT官方用test作val
+    # 处理images
+    os.makedirs(f'{dst}/images/{split}', exist_ok=True)
+    os.makedirs(f'{dst}/labels/{split}', exist_ok=True)
 
-        # 创建目标目录
-        os.makedirs(img_dst, exist_ok=True)
-        os.makedirs(label_dst, exist_ok=True)
+    # 构建有效图片列表
+    labels = {f.split('.')[0] for f in os.listdir(f'{src}/labels/{split}')}
 
-        # 获取有效图片列表
-        valid_images = set(f.split('.')[0] for f in os.listdir(label_src) if f.endswith('.txt'))
-
-        # 复制有效数据
-        for img_file in tqdm(os.listdir(img_src), desc=f'Processing {split}'):
-            if img_file.split('.')[0] in valid_images:
-                # 复制图片
-                shutil.copy(
-                    src=os.path.join(img_src, img_file),
-                    dst=os.path.join(img_dst, img_file)
-                )
-                # 复制标签
-                label_file = img_file.rsplit('.', 1)[0] + '.txt'
-                shutil.copy(
-                    src=os.path.join(label_src, label_file),
-                    dst=os.path.join(label_dst, label_file)
-                )
-
-
-# 使用示例
-clean_dataset(
-    src_root="/home/a10/ZH/ultralytics-main/datasets/UAVDT",
-    dst_root="/home/a10/ZH/ultralytics-main/datasets/UAVDT"
-)
+    # 复制有效数据
+    for img in os.listdir(f'{src}/images/{split}'):
+        if img.split('.')[0] in labels:
+            shutil.copy(
+                src=f'{src}/images/{split}/{img}',
+                dst=f'{dst}/images/{split}/{img}'
+            )
+            shutil.copy(
+                src=f'{src}/labels/{split}/{img.replace(os.path.splitext(img)[1], ".txt")}',
+                dst=f'{dst}/labels/{split}/{img.replace(os.path.splitext(img)[1], ".txt")}'
+            )
