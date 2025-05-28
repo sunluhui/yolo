bcb86e8cdad5cd2053092b7c97113ec8379ec253
slh <2632770017@qq.com>
2025-05-28 20:03:08 +0800

    updatev12

--- a.py
+++ a.py
@@ -1,7 +1,3 @@
-import openxlab
-openxlab.login(ak='pbyzd3eawggmon03lrwv', sk='nmoqn3gg92ldjbodnw2nnmywnmz1lrew6yawpezx') # 进行登录，输入对应的AK/SK，可在个人中心查看AK/SK
-
-
-from openxlab.dataset import get
-get(dataset_repo='OpenDataLab/SeaPerson', target_path='/home/a10/ZH/ultralytics-main/datasets/') # 数据集下载
-
+import pandas as pd
+df = pd.read_csv('annotations.txt')
+print(df.isnull().sum())
