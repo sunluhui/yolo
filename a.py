ff929e1f641f9f51d15e48728d674bf582bf25f1
slh <2632770017@qq.com>
2025-05-28 15:02:07 +0800

    updatev12

--- a.py
+++ a.py
@@ -1,19 +1,8 @@
-import os
-from glob import glob
+import openxlab
+openxlab.login(ak='pbyzd3eawggmon03lrwv', sk='nmoqn3gg92ldjbodnw2nnmywnmz1lrew6yawpezx') # 进行登录，输入对应的AK/SK，可在个人中心查看AK/SK
 
-# 图片和标签路径
-img_dir = "/home/a10/ZH/ultralytics-main/datasets/UAVDT/train/images"
-label_dir = "/home/a10/ZH/ultralytics-main/datasets/UAVDT/train/labels"
 
-# 获取所有图片和标签文件名（不带扩展名）
-img_files = [os.path.splitext(f)[0] for f in os.listdir(img_dir) if f.endswith('.jpg')]
-label_files = [os.path.splitext(f)[0] for f in os.listdir(label_dir) if f.endswith('.txt')]
 
-# 找出无标签的图片
-no_label_imgs = set(img_files) - set(label_files)
+from openxlab.dataset import get
+get(dataset_repo='OpenDataLab/UAVDT', target_path='/home/a10/slh/yolo/datasets/') # 数据集下载
 
-# 删除无标签的图片
-for img_name in no_label_imgs:
-    img_path = os.path.join(img_dir, img_name + '.jpg')
-    os.remove(img_path)
-    print(f"Deleted: {img_path}")
