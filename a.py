4ecfda01d5e024df2a3c5c9cc62c60ee5a51861f
slh <2632770017@qq.com>
2025-05-28 18:20:54 +0800

    updatev12

--- a.py
+++ a.py
@@ -2,7 +2,6 @@ import openxlab
 openxlab.login(ak='pbyzd3eawggmon03lrwv', sk='nmoqn3gg92ldjbodnw2nnmywnmz1lrew6yawpezx') # 进行登录，输入对应的AK/SK，可在个人中心查看AK/SK
 
 
-
 from openxlab.dataset import get
-get(dataset_repo='OpenDataLab/UAVDT', target_path='/home/a10/slh/yolo/datasets/') # 数据集下载
+get(dataset_repo='OpenDataLab/SeaPerson', target_path='/path/to/local/folder/') # 数据集下载
 
