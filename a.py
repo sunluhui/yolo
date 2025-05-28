427bf0bddf544e97b2cd5848f1869656fd7183e4
slh <2632770017@qq.com>
2025-05-28 18:25:41 +0800

    updatev12

--- a.py
+++ a.py
@@ -3,5 +3,5 @@ openxlab.login(ak='pbyzd3eawggmon03lrwv', sk='nmoqn3gg92ldjbodnw2nnmywnmz1lrew6y
 
 
 from openxlab.dataset import get
-get(dataset_repo='OpenDataLab/SeaPerson', target_path='/path/to/local/folder/') # 数据集下载
+get(dataset_repo='OpenDataLab/SeaPerson', target_path='/home/a10/ZH/ultralytics-main/datasets/') # 数据集下载
 
