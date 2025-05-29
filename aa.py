ae6471e59f4ab3c51ebaa35bc0b22b5f3734dadd
slh <2632770017@qq.com>
2025-05-29 14:26:37 +0800

    updatev12

---
+++ aa.py
@@ -0,0 +1,16 @@
+import os
+
+# 配置路径
+img_dir = "/home/a10/slh/yolo/datasets/UAVDT/cleaned_dataset"  # 清洗后的图片目录
+label_dir = "/home/a10/slh/yolo/datasets/UAVDT/train/labels"  # 标签目录
+
+# 获取清洗后保留的图片名（不带扩展名）
+valid_images = {os.path.splitext(f)[0] for f in os.listdir(img_dir)}
+
+# 遍历标签目录并删除无效标签
+for label_file in os.listdir(label_dir):
+    label_name = os.path.splitext(label_file)[0]  # 去掉.txt后缀
+
+    if label_name not in valid_images:
+        os.remove(os.path.join(label_dir, label_file))
+        print(f"Deleted label: {label_file}")
