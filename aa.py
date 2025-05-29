4cd9e3492ca69794c2fcbb5b797c94bcbc5b6499
slh <2632770017@qq.com>
2025-05-29 14:41:11 +0800

    updatev12

--- aa.py
+++ aa.py
@@ -2,7 +2,7 @@ import os
 
 # 配置路径
 img_dir = "/home/a10/slh/yolo/datasets/UAVDT/cleaned_dataset"  # 清洗后的图片目录
-label_dir = "/home/a10/slh/yolo/datasets/UAVDT/train/labels"  # 标签目录
+label_dir = "/home/a10/slh/yolo/datasets/UAVDT/test/labels"  # 标签目录
 
 # 获取清洗后保留的图片名（不带扩展名）
 valid_images = {os.path.splitext(f)[0] for f in os.listdir(img_dir)}
