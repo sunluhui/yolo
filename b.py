c7133df5873982d7a85bff94055189dedc513d25
slh <2632770017@qq.com>
2025-05-29 16:40:53 +0800

    updatev12

--- b.py
+++ b.py
@@ -4,7 +4,7 @@ import os
 folder_path = "/home/a10/slh/yolo/datasets/OpenDataLab___UAVDT/raw/UAV-benchmark-M"
 folder_path1 = os.path.join(folder_path, 'gt')  # 存放分割后的txt文件的路径
 folder_path2 = os.path.join(folder_path, 'images')  # 照片的路径，主要用于统计有多少张照片，方便创建txt文件
-folder_path3 = os.path.join(folder_path, 'gt', 'M0101_gt_whole.txt')  # 存放标签的txt文件
+folder_path3 = os.path.join(folder_path, 'gt', 'labels')  # 存放标签的txt文件
 
 photo_count = 0
 # 判断图片数量
