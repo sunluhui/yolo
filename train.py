54330e9f1d4619dc9aeaef68e3bee9b45bab07c8
slh <2632770017@qq.com>
2025-05-30 09:47:48 +0800

    updatev12

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # 加载配置
-model = YOLO(model='yolov8n-AFPN4.yaml', task='detect')
+model = YOLO(model='yolov8-ema.yaml', task='detect')
 
 # 训练参数
 model.train(
-    data='DOTAv1.5.yaml',  # 数据集配置文件
-    epochs=200,
+    data='VisDrone.yaml',  # 数据集配置文件
+    epochs=300,
     imgsz=640,
-    batch=6,  # 可设置为更小的，占据更少的空间。
+    batch=16,  # 可设置为更小的，占据更少的空间。
 )
 
