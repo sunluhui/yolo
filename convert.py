dcee71a7371d6449807078b0a4d960b2ffb7f045
sunluhui <2632770017@qq.com>
2025-06-19 21:36:04 +0800

    updatev12

--- convert.py
+++ convert.py
@@ -92,7 +92,7 @@ def convert_obb_to_yolo(obb_label_dir, image_dir, output_dir, class_list):
 
 if __name__ == "__main__":
     # 配置路径和类别
-    OBB_LABEL_DIR = "/home/a10/slh/yolo/DOTAv1.5/labels"  # OBB标注目录
+    OBB_LABEL_DIR = "/home/a10/slh/DOTAv1.5/labels"  # OBB标注目录
     IMAGE_DIR = "/home/a10/slh/yolo/DOTAv1.5/images"  # 图像目录
     OUTPUT_DIR = "/home/a10/slh/yolo/DOTAv1.5/yolo_labels"  # 输出目录
 
