6489c7be73947f7dc2fcd91a91e1488b0b95f6fc
sunluhui <2632770017@qq.com>
2025-07-12 10:16:23 +0800

    updatev12

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ model = YOLO(model='yolov12.yaml', task='detect')
 
 # 训练参数
 model.train(
-    data='TinyPerson.yaml',  # 数据集配置文件
-    epochs=200,
+    data='coco.yaml',  # 数据集配置文件
+    epochs=400,
     imgsz=640,
-    batch=4,  # 可设置为更小的，占据更少的空间。
+    batch=32,  # 可设置为更小的，占据更少的空间。
 )
 
