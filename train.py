21baae95accdb484b854ef5d737e394d07a099f4
sunluhui <2632770017@qq.com>
2025-09-24 18:31:31 +0800

    最新版

--- train.py
+++ train.py
@@ -2,7 +2,8 @@ from ultralytics import YOLO
 
 # 加载配置
 model = YOLO(model='yolov8-rtdetr.yaml', task='detect')
-
+model.info()
+model.model.use_dn = False
 # 训练参数
 model.train(
     data='VisDrone.yaml',  # 数据集配置文件
