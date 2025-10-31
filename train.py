0b6da779326035e76145b123e7655dee53ca6e89
sunluhui <2632770017@qq.com>
2025-10-31 19:34:04 +0800

    最新版

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # 加载配置
-model = YOLO(model='yolov8-rtdetr.yaml', task='detect')
+model = YOLO(model='yolov8-CoordAtt.yaml', task='detect')
 
 # 训练参数
-model.train(
+model.val(
     data='VisDrone.yaml',  # 数据集配置文件
-    epochs=300,
+    epochs=20,
     imgsz=640,
     batch=8,  # 可设置为更小的，占据更少的空间。
 )
