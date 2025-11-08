2111c910526e4488302f49a8f8da73da5bcb017e
sunluhui <2632770017@qq.com>
2025-11-08 10:22:49 +0800

    最新版

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
+import torch
+
 from ultralytics import YOLO
 
 # 加载配置xaa
 model = YOLO(model='yolov8-p2.yaml', task='detect')
-
+torch.use_deterministic_algorithms(False)
 # 训练参数
 model.train(
     data='VisDrone.yaml',  # 数据集配置文件
