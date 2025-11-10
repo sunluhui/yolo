c3305c1ee81600cd199356f9972359521581354f
sunluhui <2632770017@qq.com>
2025-11-10 21:31:29 +0800

    最新版

--- train.py
+++ train.py
@@ -7,9 +7,9 @@ model = YOLO(model='yolov8-p2.yaml', task='detect')
 torch.use_deterministic_algorithms(False)
 # 训练参数
 model.train(
-    data='VisDrone.yaml',  # 数据集配置文件
+    data='DOTAv1.5.yaml',  # 数据集配置文件
     epochs=300,
-    imgsz=640,
-    batch=8,  # 可设置为更小的，占据更少的空间。
+    imgsz=512,
+    batch=2,  # 可设置为更小的，占据更少的空间。
 )
 
