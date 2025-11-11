8d6f65ab79cd45d3244072b94fdaf996fcd7dc9c
sunluhui <2632770017@qq.com>
2025-11-11 11:00:31 +0800

    最新版

--- train.py
+++ train.py
@@ -3,13 +3,13 @@ import torch
 from ultralytics import YOLO
 
 # 加载配置xaa
-model = YOLO(model='yolov8-p2.yaml', task='detect')
+model = YOLO(model='yolo11-cls.yaml', task='detect')
 torch.use_deterministic_algorithms(False)
 # 训练参数
 model.train(
-    data='TinyPerson.yaml',  # 数据集配置文件
-    epochs=200,
+    data='VisDrone.yaml',  # 数据集配置文件
+    epochs=300,
     imgsz=640,
-    batch=4,  # 可设置为更小的，占据更少的空间。
+    batch=8,  # 可设置为更小的，占据更少的空间。
 )
 
