6bd75db79bae0ba4d134723ff559ec997036a507
sunluhui <2632770017@qq.com>
2025-11-11 20:32:52 +0800

    最新版

--- ultralytics/nn/modules/bifpn.py
+++ ultralytics/nn/modules/bifpn.py
@@ -5,45 +5,40 @@ import torch.nn.functional as F
 from .conv import Conv
 
 
-class BiFPN_Concat2(nn.Module):
-    """Robust BiFPN Block with Automatic Channel Alignment"""
-
-    def __init__(self, c2, num_inputs=2, eps=1e-4):
-        """
-        Args:
-            c2 (int): 输出通道数
-            num_inputs (int): 输入特征图数量
-            eps (float): 防止除零的小常数
-        """
+class Fusion(nn.Module):
+    def __init__(self, inc_list, fusion='bifpn') -> None:
         super().__init__()
-        self.eps = eps
-        self.num_inputs = num_inputs
-        self.out_channels = c2
 
-        # 为每个输入创建通道对齐层
-        self.align_convs = nn.ModuleList([
-            Conv(c2, c2, k=1, s=1, act=False) for _ in range(num_inputs)
-        ])
+        assert fusion in ['weight', 'adaptive', 'concat', 'bifpn', 'SDI']
+        self.fusion = fusion
 
-        # 可学习权重
-        self.w = nn.Parameter(torch.ones(num_inputs, dtype=torch.float32), requires_grad=True)
+        if self.fusion == 'bifpn':
+            self.fusion_weight = nn.Parameter(torch.ones(len(inc_list), dtype=torch.float32), requires_grad=True)
+            self.relu = nn.ReLU()
+            self.epsilon = 1e-4
+        elif self.fusion == 'SDI':
+            self.SDI = self.SDI(inc_list)
+        else:
+            self.fusion_conv = nn.ModuleList([Conv(inc, inc, 1) for inc in inc_list])
 
-        # 特征转换层
-        self.conv = Conv(c2, c2, k=3, s=1, p=1, g=1, act=True)
+            if self.fusion == 'adaptive':
+                self.fusion_adaptive = Conv(sum(inc_list), len(inc_list), 1)
 
     def forward(self, x):
-        """
-        处理输入特征图，确保通道数一致
-        """
-        # 对齐所有输入特征图到统一通道数
-        aligned = [self.align_convs[i](x[i]) for i in range(self.num_inputs)]
-
-        # 计算归一化权重
-        weights = F.relu(self.w)
-        weights = weights / (weights.sum() + self.eps)
-
-        # 加权特征融合
-        fused = sum(weights[i] * aligned[i] for i in range(self.num_inputs))
-
-        # 特征转换
-        return self.conv(fused)
+        if self.fusion in ['weight', 'adaptive']:
+            for i in range(len(x)):
+                x[i] = self.fusion_conv[i](x[i])
+        if self.fusion == 'weight':
+            return torch.sum(torch.stack(x, dim=0), dim=0)
+        elif self.fusion == 'adaptive':
+            fusion = torch.softmax(self.fusion_adaptive(torch.cat(x, dim=1)), dim=1)
+            x_weight = torch.split(fusion, [1] * len(x), dim=1)
+            return torch.sum(torch.stack([x_weight[i] * x[i] for i in range(len(x))], dim=0), dim=0)
+        elif self.fusion == 'concat':
+            return torch.cat(x, dim=1)
+        elif self.fusion == 'bifpn':
+            fusion_weight = self.relu(self.fusion_weight.clone())
+            fusion_weight = fusion_weight / (torch.sum(fusion_weight, dim=0) + self.epsilon)
+            return torch.sum(torch.stack([fusion_weight[i] * x[i] for i in range(len(x))], dim=0), dim=0)
+        elif self.fusion == 'SDI':
+            return self.SDI(x)
