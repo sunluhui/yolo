bc519a3d93087a23a1d20877bd447df22ce16d25
sunluhui <2632770017@qq.com>
2025-11-13 20:42:47 +0800

    最新版

--- ultralytics/nn/Extramodule/__init__.py
+++ ultralytics/nn/Extramodule/__init__.py
@@ -2,7 +2,7 @@ from .SCConv import *
 from .MLKA import *
 from .mamba_yolo import *
 from .AKConv import *
-from .MLLA import *
+
 from .swinTransFormer import *
-from .build import *
+
 from .HWD import *
