015479c4328a553e75a64be1665fa450de5bfb24
sunluhui <2632770017@qq.com>
2025-11-16 22:17:06 +0800

    最新版

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch
 from ultralytics import YOLO
 from ultralytics import RTDETR
 # 加载配置xaa
-model = YOLO(model='yolo11-cls.yaml', task='detect')
+model = RTDETR(model='me.yaml', task='detect')
 torch.use_deterministic_algorithms(False)
 # 训练参数
 model.train(
@@ -13,8 +13,8 @@ model.train(
     epochs=300,
     imgsz=640,
     batch=8, # 可设置为更小的，占据更少的空间。下面的为rtdetr的配置。还得修改训练和测试集的文件头
-    #optimizer='AdamW',
-    #lr0=0.0001,
-    #weight_decay=0.0001,
+    optimizer='AdamW',
+    lr0=0.0001,
+    weight_decay=0.0001,
 )
 
