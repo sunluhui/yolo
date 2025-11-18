0caa59eef466f1771c707270ab37da8958a091b5
sunluhui <2632770017@qq.com>
2025-11-18 11:47:20 +0800

    最新版

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 
 from ultralytics import YOLO
 from ultralytics import RTDETR
-# 加载配置xaa
+# 加载配置xaaa
 model = RTDETR(model='me.yaml')#, task='detect')
 torch.use_deterministic_algorithms(False)
 # 训练参数
