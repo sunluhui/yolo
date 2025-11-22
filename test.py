44d65715628b6644d59820860367f880b28d4187
sunluhui <2632770017@qq.com>
2025-11-22 08:41:01 +0800

    最新版

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from ultralytics import RTDETR
 from ultralytics import YOLO
 # 加载训练好的模型（必须使用RTDETR类加载RT-DETR模型）
-model = RTDETR('runs/detect/train425/weights/best.pt')  # 每次训练完进行测试时，必须修改测试模型的路径
-
+model = RTDETR('runs/detect/train428/weights/best.pt')  # 每次训练完进行测试时，必须修改测试模型的路径
+model.val(device=0)  # 明确指定使用GPU 0
 # 在测试集上进行评估
 metrics = model.val(
     data='VisDrone.yaml',  # 数据集配置文件路径
