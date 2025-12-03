f73e992ac5e0674b44159a464538f2b9593987f5
sunluhui <2632770017@qq.com>
2025-12-03 12:44:16 +0800

    最新版

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ torch.use_deterministic_algorithms(False)
 model.train(
     data='DOTAv1.5.yaml',  # 数据集配置文件
     epochs=200,
-    imgsz=640,
+    imgsz=512,#640,
     batch=4, # 8主数据集, # 可设置为更小的，占据更少的空间。下面的为rtdetr的配置。还得修改训练和测试集的文件头
     optimizer='AdamW',
     lr0=0.0001,
