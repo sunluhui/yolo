1e7a44fd8817a8cc85010871d948de8ce95f0eac
sunluhui <2632770017@qq.com>
2025-12-04 14:56:43 +0800

    最新版

--- train.py
+++ train.py
@@ -5,14 +5,14 @@ import torch
 from ultralytics import YOLO
 from ultralytics import RTDETR
 # 加载配置xaaaaa
-model = RTDETR(model='EMA.yaml')#, task='detect')
+model = YOLO(model='yolo11.yaml', task='detect')
 torch.use_deterministic_algorithms(False)
 # 训练参数
 model.train(
-    data='TinyPerson.yaml',  # 数据集配置文件
-    epochs=200,
+    data='VisDrone.yaml',  # 数据集配置文件
+    epochs=400,
     imgsz=640,
-    batch=4, # 8主数据集, # 可设置为更小的，占据更少的空间。下面的为rtdetr的配置。还得修改训练和测试集的文件头
+    batch=8, # 8主数据集, # 可设置为更小的，占据更少的空间。下面的为rtdetr的配置。还得修改训练和测试集的文件头
     optimizer='AdamW',
     lr0=0.0001,
     weight_decay=0.0001,
